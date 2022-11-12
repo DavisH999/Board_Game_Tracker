23bd28cd5f8d39a6387965d0f9618433a51d0bca
Daniel <sut>
2022-11-12 15:36:57 -0800

    SetTheme Activity almost done. Minor issues with a toast

--- app/src/main/java/ca/cmpt276/project_7f/SetTheme.java
+++ app/src/main/java/ca/cmpt276/project_7f/SetTheme.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SetTheme extends AppCompatActivity {
+public class SetTheme extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,30 @@ public class SetTheme extends AppCompatActivity {
         setContentView(R.layout.activity_set_theme);
 
         Spinner spinner_theme = findViewById(R.id.spinner_theme);
-        //ArrayAdapter<CharSequence>
+        ImageView btn_back = findViewById(R.id.set_theme_back_button);
+
+        //Spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.achievement_levels, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_theme.setAdapter(adapter);
+        String theme = spinner_theme.getSelectedItem().toString();
+
+        btn_back.setOnClickListener(v->onBackClick());
+        //spinner_theme.setOnClickListener(v->spinnerCLick());
     }
-
-
 
     public static Intent makeIntent(Context context, int index) {
         Intent intent = new Intent(context, SetTheme.class);
         intent.putExtra("indexOfConfigInList", index);
         return intent;
     }
+
+    public void onBackClick() {
+        finish();
+    }
+
+    public void spinnerCLick() {
+        //Toast.makeText(SetTheme.this, "I chose you pikachu!", Toast.LENGTH_LONG).show();
+    }
+
 }
