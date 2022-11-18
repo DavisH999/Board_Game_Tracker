ba84a98188feaa570a009322683655346745ca9a
Dongwei Han <handongwei917@hotmail.com>
2022-11-17 17:35:25 -0800

    Updated Junit dependencies and added a piece of magic code to make Junit5 work.

---
+++ app/src/test/java/ca/cmpt276/project_7f/model/ConfigTest.java
@@ -0,0 +1,32 @@
+package ca.cmpt276.project_7f.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ConfigTest {
+
+    @Test
+    void getName() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getPoorScore() {
+    }
+
+    @Test
+    void setPoorScore() {
+    }
+
+    @Test
+    void getGreatScore() {
+    }
+
+    @Test
+    void setGreatScore() {
+    }
+}
