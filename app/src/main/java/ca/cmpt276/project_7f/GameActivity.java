d4049a7684b912cb1e968645114c72d11be46e50
Dongwei Han <handongwei917@hotmail.com>
2022-11-17 19:18:04 -0800

    Refactored the code.

--- app/src/main/java/ca/cmpt276/project_7f/GameActivity.java
+++ app/src/main/java/ca/cmpt276/project_7f/GameActivity.java
@@ -275,8 +275,6 @@ public class GameActivity extends AppCompatActivity {
         MessageFragment messageFragment = new MessageFragment();
         messageFragment.setter(configName,indexOfGameInList,isAddMode);
         messageFragment.show(supportFragmentManager,"MessageFragment");
-
-
     }
 
     private void playSounds() {
