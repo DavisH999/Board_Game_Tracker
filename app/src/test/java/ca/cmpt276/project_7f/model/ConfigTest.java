6db3755337e320cbdf73f8ab5b630d2bce5edf34
Dongwei Han <handongwei917@hotmail.com>
2022-11-17 17:55:47 -0800

    Tested toString method.

--- app/src/test/java/ca/cmpt276/project_7f/model/ConfigTest.java
+++ app/src/test/java/ca/cmpt276/project_7f/model/ConfigTest.java
@@ -6,17 +6,32 @@ import org.junit.jupiter.api.Test;
 class ConfigTest {
 
     @Test
+    void testToString() {
+        Config config = new Config();
+        config.setName("NAME");
+        config.setGreatScore(100);
+        config.setPoorScore(0);
+        String toString1 = config.toString();
+        String toString2 = "Config{" +
+                "configName='" + config.getName() + '\'' +
+                ", poorScore=" + config.getPoorScore() +
+                ", greatScore=" + config.getGreatScore() +
+                '}';
+        assertEquals(toString1,toString2);
+    }
+
+    @Test
     void getName() {
         Config config = new Config();
-        config.setName("TEST");
-        assertEquals("TEST",config.getName());
+        config.setName("NAME");
+        assertEquals("NAME",config.getName());
     }
 
     @Test
     void setName() {
         Config config = new Config();
-        config.setName("TEST");
-        assertEquals("TEST",config.getName());
+        config.setName("NAME");
+        assertEquals("NAME",config.getName());
     }
 
     @Test
